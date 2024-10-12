b0be2cc6546320325eb4d942dc783b176fa51147
heluani <ismael.heluani@wipo.int>
2024-10-12 09:19:04 +0200

    Adding data configuration

---
+++ src/main/resources/db/migration/V2__insert_initial_products.sql
@@ -0,0 +1,6 @@
+INSERT INTO PRODUCTS (ean, name, provider, destination) VALUES
+   ('8437008459059', 'Product 1', 'Hacendado', 'Tiendas Mercadona España'),
+   ('8480000160072', 'Product 2', 'Another Provider', 'Tiendas Mercadona España'),
+   ('8437008459066', 'Product 3', 'Hacendado', 'Almacenes'),
+   ('8480000160079', 'Product 4', 'Another Provider', 'Oficinas Mercadona'),
+   ('8437008459073', 'Product 5', 'Hacendado', 'Colmenas');
